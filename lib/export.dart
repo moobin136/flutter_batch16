63f384269a2fd33aaea024f11e1960808068ddb9
moobin136 <moobinislam3@gmail.com>
2026-04-29 19:47:31 +0600

    new add

--- lib/export.dart
+++ lib/export.dart
@@ -1,5 +1,6 @@
 export 'package:flutter/material.dart';
 export 'package:flutter_batch16/app.dart';
-export 'model/home/screen/home.dart';
-export 'package:flutter_batch16/core/global_wigets/cm_text.dart';
-export 'package:flutter_batch16/core/global_wigets/cm_appbar.dart';
+export 'package:flutter_batch16/screen/product_list_screen.dart';
+export 'package:flutter_batch16/wigets/product_item.dart';
+export 'package:flutter_batch16/screen/update_product_scree.dart';
+export 'package:flutter_batch16/screen/add_new_product_scree.dart';
