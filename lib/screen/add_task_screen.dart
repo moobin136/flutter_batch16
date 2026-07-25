dbd63f96e50777d847d6516b5c5ae88fd80acb6c
mobin136 <moobinislam3@gmail.com>
2026-07-25 12:54:13 +0600

    no change

--- lib/screen/add_task_screen.dart
+++ lib/screen/add_task_screen.dart
@@ -1,7 +1,5 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter_batch16/export.dart';
 import 'package:flutter_batch16/routes.dart';
-import 'package:flutter_batch16/utils/app_colors.dart';
 
 class AddTaskScreen extends StatefulWidget {
   const AddTaskScreen({super.key});
@@ -68,6 +66,4 @@ class _AddTaskScreenState extends State<AddTaskScreen> {
       ),
     );
   }
-
-
 }
