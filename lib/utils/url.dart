a0f97ab10bc7c4ddc12234166b427f7bc8600a55
mobin136 <moobinislam3@gmail.com>
2026-09-10 12:49:38 +0600

    url not change

--- lib/utils/url.dart
+++ lib/utils/url.dart
@@ -3,8 +3,6 @@ class AppUrl {
       'https://crud-api-ostad-live.onrender.com/api/v1';
   static const String readProduct = '$_baseUrl/ReadProduct';
   static const String createProduct = '$_baseUrl/CreateProduct';
-
   static String deleteProduct(String? id) => '$_baseUrl/DeleteProduct/$id';
-
   static String updateProduct(String? id) => '$_baseUrl/UpdateProduct/$id';
 }
